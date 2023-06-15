b60787b3762cb0127a64fb2de7a0d623b905a941
white_key <112360339+Clue03@users.noreply.github.com>
2023-06-15 17:44:46 +0900

    Add files via upload

--- 202235297_이세영.js
+++ 202235297_이세영.js
@@ -6,25 +6,82 @@ $(document).ready(function(){
     });
 });
 
-
 $(document).ready(function(){
+    var width = $('[data-role="slider"]').attr('data-width');
+    var height = $('[data-role="slider"]').attr('data-height');
+    var count = $('[data-role="slider"] div.item').length;
+    var count1 = $('[data-role="slider"] div.item1').length;
 
-})
+    $('[data-role="slider"]').css({
+        position: 'relative',
+        overflow: 'hidden',
+        width: width,
+        height: height
+    }).find('.container').css({
+        position: 'absolute',
+        overflow: 'hidden',
+        width: width * count
+    }).find('.item').css({
+        width: width,
+        height: height,
+        float: 'left'
+    });
 
-$(document).ready(function(){
+    $('[data-role="slider"]').css({
+        position: 'relative',
+        overflow: 'hidden',
+        width: width,
+        height: height
+    }).find('.container1').css({
+        position: 'absolute',
+        overflow: 'hidden',
+        width: width * count1
+    }).find('.item1').css({
+        width: width,
+        height: height,
+        float: 'left'
+    });
+
+    var currentPage = 0;
+    var currentPage1 = 0;
+    var changePage = function(){
+        $('[data-role="slider"] > .container').animate({
+            left: -currentPage * width
+        }, 500);
+    };
+    var changePage1 = function(){
+        $('[data-role="slider"]>.container1').animate({
+            left: -currentPage1 * width
+        }, 500);
+    };
+
+    $('#left-button').click(function(){
+        if(currentPage > 0){
+            currentPage = currentPage - 1;
+            changePage();
+        }
+    });
     
-})
-
-$('#left-button').click(function(){
-    if(currentPage > 0){
-        currentPage = currentPage - 1;
-        changePage();
-    }
+    $('#left-button1').click(function(){
+        if(currentPage1 > 0){
+            currentPage1 = currentPage1 - 1;
+            changePage1();
+        }
+    });
+    
+    $('#right-button').click(function(){
+        if(currentPage < count - 1){
+            currentPage = currentPage + 1;
+            changePage();
+        }
+    });
+    
+    $('#right-button1').click(function(){
+        if(currentPage1 < count1 - 1){
+            currentPage1 = currentPage1 + 1;
+            changePage1();
+        }
+    });
 });
 
-$('#right-button').click(function(){
-    if(currentPage < count - 1){
-        currentPage = currentPage + 1;
-        changePage();
-    }
-});
+
